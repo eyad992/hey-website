85b833d8499239ebf6111f20eeb2d070a630bba1
eyad992 <eyadgarro410@gmail.com>
2022-08-07 23:54:50 +0200

    new-version

---
+++ src/components/Navbar.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import styled, { css } from 'styled-components/macro';
+import { Link } from 'react-router-dom';
+import { menuData } from '../data/MenuData';
+
+const Nav = styled.nav`
+  height: 60px;
+  display: flex;
+  justify-content: space-between;
+  padding: 1rem 2rem;
+  z-index: 100;
+  position: fixed;
+  width: 100%;
+  background: red;
+`;
+
+const NavLink = css`
+  color: #fff;
+  display: flex;
+  align-items: center;
+  padding: 0 1rem;
+  height: 100%;
+  cursor: pointer;  
+`;
+
+const Logo = styled(Link)`
+  ${NavLink}
+  font-style: italic;
+`;
+
+const MenuBars = styled.i``;
+const NavMenu = styled.div``;
+const NavMenuLinks = styled(Link)`
+  color: #fff;
+`;
+function Navbar() {
+  return (
+    <Nav>
+      <Logo to=''>ELIXR</Logo>
+      <MenuBars />
+      <NavMenu>
+        {menuData.map((item, index) => (
+          <NavMenuLinks to={item.link} key={index}>
+            {item.title}
+          </NavMenuLinks>
+        ))}
+      </NavMenu>
+    </Nav>
+  )
+}
+
+export default Navbar
