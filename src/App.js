6d0db14f280f9c69805681331545bc0048697a0a
eyad992 <eyadgarro410@gmail.com>
2022-08-18 01:07:31 +0200

    changes-august18

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Navbar from './components/Navbar';
+import GlobalStyle from './globalStyles';
+import Dropdown from './components/Dropdown';
 
 function App() {
   return (
    <>
+    <GlobalStyle />
     <Navbar/>
+    <Dropdown />
    </>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,18 @@ import React from 'react';
 import styled, { css } from 'styled-components/macro';
 import { Link } from 'react-router-dom';
 import { menuData } from '../data/MenuData';
+import { Button } from './Button';
+import { FaBars } from 'react-icons/fa';
 
+const NavBtn = styled.div`
+  display: flex;
+  align-items: center;
+  margin-right: 24px;
+
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`;
 const Nav = styled.nav`
   height: 60px;
   display: flex;
@@ -20,7 +31,8 @@ const NavLink = css`
   align-items: center;
   padding: 0 1rem;
   height: 100%;
-  cursor: pointer;  
+  cursor: pointer;
+  text-dicoration: none;
 `;
 
 const Logo = styled(Link)`
@@ -28,10 +40,29 @@ const Logo = styled(Link)`
   font-style: italic;
 `;
 
-const MenuBars = styled.i``;
-const NavMenu = styled.div``;
+const MenuBars = styled(FaBars)`
+  display: none;
+  @media screen and (max-width: 768px) {
+    display: block;
+    width: 40px;
+    height: 40px;
+    position: absolute;
+    top: 0;
+    right: 0;
+    transform: translate(-50%, 25%);
+  }
+`;
+const NavMenu = styled.div`
+  display: flex;
+  align-items: center;
+  margin-right: -48px;
+
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`;
 const NavMenuLinks = styled(Link)`
-  color: #fff;
+  ${NavLink};
 `;
 function Navbar() {
   return (
@@ -45,6 +76,9 @@ function Navbar() {
           </NavMenuLinks>
         ))}
       </NavMenu>
+      <NavBtn>
+        <Button to="/contact" primary='true'>Contact Us</Button>
+      </NavBtn>
     </Nav>
   )
 }
