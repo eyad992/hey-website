766071e1635dc8dca103092406ff96317f6122d7
eyad992 <eyadgarro410@gmail.com>
2022-08-29 02:35:41 +0200

    changes and add form

--- src/App.js
+++ src/App.js
@@ -6,18 +6,22 @@ import { BrowserRouter as Router, Routes, Route, HashRouter } from 'react-router
 import Services from './components/pages/Services';
 import Products from './components/pages/Products';
 import SignUp from './components/pages/SignUp';
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return (
     <>
       <HashRouter>
         <Navbar />
+        <ScrollToTop>
         <Routes>
           <Route path='/' exact element={<Home/>} />
           <Route path='/services'exact element={<Services/>} />
           <Route path='/products'exact element={<Products/>} />
           <Route path='/sign-up' exact element={<SignUp/>} />
         </Routes>
+        </ScrollToTop>
+
       </HashRouter>
     </>
   );
--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -1,12 +1,68 @@
 import React from 'react';
 import '../../App.css';
 import Footer from '../Footer';
+import Form from 'react-bootstrap/Form';
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import Button from 'react-bootstrap/Button';
+import '../styles/Signup.scss';
+
+
 
 
 export default function SignUp() {
   return (
     <>
-    <h1 className='sign-up'>LIKE & SUBSCRIBE</h1>
+      <div className="local-bootstrap">
+      <div className="container">
+      <h1 className="title mb-3 mt-5">Contact Us</h1>
+     {/* <Form className="ml-5">
+      <Form.Control type="text" placeholder="Normal text" />
+      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="name@example.com" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+        <Form.Label>Example textarea</Form.Label>
+        <Form.Control as="textarea" rows={3} />
+      </Form.Group>
+    </Form> */}
+    <Form>
+    <FloatingLabel
+        controlId="floatingTextarea"
+        label="Name"
+        className="mb-3"
+      >
+        <Form.Control as="textarea" placeholder="Leave a comment here" />
+      </FloatingLabel>
+      <FloatingLabel
+        controlId="floatingInput"
+        label="Email address"
+        className="mb-3"
+      >
+        <Form.Control type="email" placeholder="name@example.com" />
+      </FloatingLabel>
+      <FloatingLabel
+        controlId="floatingTextarea"
+        label="Subject"
+        className="mb-3"
+      >
+        <Form.Control as="textarea" placeholder="Leave a comment here" />
+      </FloatingLabel>
+      <FloatingLabel controlId="floatingTextarea2" label="Message">
+        <Form.Control
+          as="textarea"
+          placeholder="Leave a comment here"
+          style={{ height: '200px' }}
+          className="mb-3"
+        />
+      </FloatingLabel>
+      <Button variant="primary" type="submit" className="mb-5">
+        Submit
+      </Button>
+      </Form>
+    </div>
+    </div>
+  
     <Footer />
     </>
   );
